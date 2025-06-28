6636491c81073def0b5e3e56d892024e203c9397
Ollsy <olesyantonovich@gmail.com>
2025-06-28 12:01:29 +0200

    Update mybottelegram.py

--- mybottelegram.py
+++ mybottelegram.py
@@ -9,6 +9,28 @@ SOURCE_TARGET_CHAT_ID_DICT = get_chats_id_info('blacklist.txt', 'clearlist.txt')
 
 bot = telebot.TeleBot(TOKEN)
 
+login as: root
+root@91.240.85.229's password:
+Welcome to Ubuntu 24.04.2 LTS (GNU/Linux 6.8.0-50-generic x86_64)
+
+ * Documentation:  https://help.ubuntu.com
+ * Management:     https://landscape.canonical.com
+ * Support:        https://ubuntu.com/pro
+Last login: Sat Jun 28 11:52:24 2025 from 212.58.120.172
+root@olesyantonovich:~# tmux attach
+[exited]
+root@olesyantonovich:~# cat telegramMP/mybottelegram.py
+import telebot
+from handler import get_chats_id_info
+# Конфигурация
+TOKEN = "7737786825:AAER3bVUc3jCLcpaK_6nFzmLN312vZAfFkc"  # Замените на токен бо                                                                                                             та
+SOURCE_TARGET_CHAT_ID_DICT = get_chats_id_info('blacklist.txt', 'clearlist.txt')
+
+# SOURCE_CHAT_ID = "-411516982"  # Отсюда будем пересылать
+# TARGET_CHAT_ID = "-1001431040944"  # Сюда будем пересылать
+
+bot = telebot.TeleBot(TOKEN)
+
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, "🤖 Бот работает! Добавьте меня в обе группы.")
@@ -28,8 +50,8 @@ def handle_photo(message):
             bot.send_photo(
                 chat_id = SOURCE_TARGET_CHAT_ID_DICT[int(message.chat.id)],
 
-                photo=message.photo[-1].file_id,  # Берем фото максимального качества
-            
+                photo=message.photo[-1].file_id,  # Берем фото максимального кач                                                                                                             ества
+
             )
         except Exception as e:
             print(f"Ошибка: {e}")
@@ -43,7 +65,7 @@ def handle_video(message):
             bot.send_video(
                 chat_id = SOURCE_TARGET_CHAT_ID_DICT[int(message.chat.id)],
                 video=message.video.file_id,
-             
+
             )
         except Exception as e:
             print(f"Ошибка: {e}")
@@ -53,5 +75,7 @@ def handle_video(message):
 # dict_of_chat = {"key1":(12121212, 25452)}
 if __name__ == "__main__":
      print("Бот запущен!")
-     bot.polling(none_stop=True)
+     bot.infinity_polling(timeout=10, long_polling_timeout=5)
 # print(dict_of_chat["key1"])
+
+
